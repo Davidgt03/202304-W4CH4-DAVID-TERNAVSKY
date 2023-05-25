4a510b1bc56fd1de9ee1a06a020848e2081afb47
david <daviidgt03@gmail.com>
2023-05-26 00:11:37 +0200

    🤡 Add layout + clickable keys + timeout to the call

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { ContextProvider } from "./context/context.provider";
+
+ReactDOM.createRoot(document.querySelector(".container") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>
+);
